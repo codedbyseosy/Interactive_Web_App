55d5d10a59f055ae109d07dd7144ba24f9599683
eseoseodion <ehodion1@sheffield.ac.uk>
2024-09-25 13:57:47 +0100

    Code can print out definition and pronunciation

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import json
 
 # syns_list = data[0]['meta']['def'][0]['sseq'][0][0]['syn_list']
 
+
 def get_definition(word, api_key):
     url = f"https://www.dictionaryapi.com/api/v3/references/thesaurus/json/{word}?key={api_key}"
 
@@ -20,6 +21,7 @@ def get_definition(word, api_key):
             # Check if definitions are available
             if 'shortdef' in entry:
                 definitions = entry['shortdef']
+                # print(data)
                 print(f"Definition for the word {word}: ")
 
                 for i, definition in enumerate(definitions, start=1):
@@ -32,8 +34,61 @@ def get_definition(word, api_key):
         print('Error:', response.status_code)
 
 
-api_key = "47a18b32-61c5-4951-ad64-1fdbbf295a5d"
+def get_synonym(word, api_key):
+    pass
+
+
+def get_antonym(word, api_key):
+    pass
+
+
+def get_pronunciation(word, api_key):
+    url = f"https://www.dictionaryapi.com/api/v3/references/collegiate/json/{word}?key={api_key}"
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+
+        # Check if any definitions are available
+        if isinstance(data, list) and len(data) > 0:
+
+            # Access the third entry in the list
+            entry = data[0]
+
+            if 'hwi' in entry:
+                headword = entry['hwi']
+
+                if 'prs' in headword:
+                    # print(f"Pronunciation exists for the word {word}.")
+
+                    new_entry = headword['prs'][0]
+
+                    if 'mw' in new_entry:
+                        mw = new_entry['mw']
+                        print(f"The word {word} is pronounced as {mw}.")
+
+                    else:
+                        print(f"The word {word} does not exist.")
+                else:
+                    print(f"The word {word} does not exist.")
+
+            else:
+                print(f"The word {word} does not exist.")
+        else:
+            print(f"The word {word} does not exist.")
+    else:
+        print('Error:', response.status_code)
+
+
+def provide_examples(word, api_key):
+    pass
+
+
+api_keys = ["47a18b32-61c5-4951-ad64-1fdbbf295a5d", "864ede40-eae9-41c8-98c3-c24c92e8dd4e"]
 
 word = "friend"
 
-get_definition(word=word, api_key=api_key)
+get_definition(word=word, api_key=api_keys[0])
+
+get_pronunciation(word=word, api_key=api_keys[1])

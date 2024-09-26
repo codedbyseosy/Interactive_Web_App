eaf87056f399c71dd33b6e079c882b4c549347f3
eseoseodion <ehodion1@sheffield.ac.uk>
2024-09-26 16:01:39 +0100

    Prints out the antonyms and the examples sentences. Changes were made to the front end

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 import requests
-import json
-
-# syns_list = data[0]['meta']['def'][0]['sseq'][0][0]['syn_list']
 
 
 def get_definition(word, api_key):
@@ -54,7 +51,32 @@ def get_synonyms(word, api_key):
 
 
 def get_antonyms(word, api_key):
-    pass
+    url = f"https://www.dictionaryapi.com/api/v3/references/thesaurus/json/{word}?key={api_key}"
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+
+        # Check if any definitions are available
+        if isinstance(data, list) and len(data) > 0:
+
+            # Access the first entry in the list
+            # ant_list = data[0]['def'][0]['sseq'][0][0][1]['ant_list']
+            #if 'ant_list' in data[0]['def'][0]['sseq'][0][0][1]:
+            ant_list = data[0]['def'][0]['sseq'][0][0][1]['ant_list']
+
+            antonyms = []
+            for ant in ant_list[0]:
+                antonyms.append(ant['wd'])
+
+            print(f"The antonyms for the word {word} are: {antonyms}")
+            # else:
+            #     print(f"No definitions for the word {word}.")
+        else:
+            print(f"No definitions for the word {word}.")
+    else:
+        print('Error:', response.status_code)
 
 
 def get_pronunciation(word, api_key):
@@ -64,6 +86,7 @@ def get_pronunciation(word, api_key):
 
     if response.status_code == 200:
         data = response.json()
+        print(data)
 
         # Check if any definitions are available
         if isinstance(data, list) and len(data) > 0:
@@ -79,15 +102,42 @@ def get_pronunciation(word, api_key):
 
 
 def provide_examples(word, api_key):
-    pass
+    url = f"https://www.dictionaryapi.com/api/v3/references/collegiate/json/{word}?key={api_key}"
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+
+        # Check if any definitions are available
+        if isinstance(data, list) and len(data) > 0:
+            t_values = []
+            entry = data[0]
+
+            if 'quotes' in entry:
+                for quote in entry['quotes']:
+                    if 't' in quote:
+                        t_values.append(quote['t'])
+                print(t_values)
+            else:
+                print(f"The word {word} does not exist.")
+
+        else:
+            print(f"The word {word} does not exist.")
+    else:
+        print('Error:', response.status_code)
 
 
 api_keys = ["47a18b32-61c5-4951-ad64-1fdbbf295a5d", "864ede40-eae9-41c8-98c3-c24c92e8dd4e"]
 
-word = "mother"
+word = "grateful"
 
 get_definition(word=word, api_key=api_keys[0])
 
 get_synonyms(word=word, api_key=api_keys[0])
 
+get_antonyms(word=word, api_key=api_keys[0])
+
 get_pronunciation(word=word, api_key=api_keys[1])
+
+provide_examples(word=word, api_key=api_keys[1])
